35dd49fb17cfd1a93e10a7a3e0527e7ae36d41a0
LuiVanCorny <ludwig.korn.96@googlemail.com>
2020-03-16 14:12:14 +0100

    created input/listview and their functionality

--- model.js
+++ model.js
@@ -1,6 +1,22 @@
+import { Person } from "./person.js";
+
 export class Model {
   constructor() {
     this.personen = [];
+    this.personen.push(new Person());
+    let person1 = new Person();
+    person1.vorname = "Ludwig";
+    person1.nachname = "Korn";
+    person1.geburtsdatum = "1997-12-12";
+
+    let person2 = new Person();
+    person2.vorname = "Nikk";
+    person2.nachname = "Bucckau";
+    person2.geburtsdatum = "1998-12-12";    
+
+    this.personen.push(person1);
+    this.personen.push(person2);
+
   }
 
   // CRUD-Methoden
--- presenter.js
+++ presenter.js
@@ -1,12 +1,33 @@
 import { StartView } from './startview.js';
 import { Model } from './model.js';
+import { ListView } from './listview.js';
+import { InputView } from './inputview.js';
 
 export class Presenter {
   constructor() {
     this.model = new Model();
-    const text = this.model.getText();
+    this._showListView();
+  }
 
-    this.view = new StartView(this);
-    this.view.render(text);
+  buttonNewClicked(){
+    this.view = new InputView(this);
+    this.view.render("Neue Person hinzufügen");
+  }
+
+  buttonSafeClick(){
+    let person = this.view.getPerson();
+    this.model.createPerson(person);
+    this._showListView();
+  }
+
+  buttonInterruptClick(){
+   this._showListView();
+  }
+
+  _showListView(){
+    this.view = new ListView(this);
+    const personen = this.model.readAllPersons();
+    this.view.render(personen);
+
   }
 }
